6bcc445c9c7e20581809e07cc8a420325ac43563
Ibrahim shamma <i.abushammah@gmail.com>
2020-01-27 11:31:00 +0200

    stitcher added

--- Emgu-Csharp/Form1.Designer.cs
+++ Emgu-Csharp/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -94,11 +95,22 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(537, 372);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 37);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Stitch";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -121,6 +133,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Emgu-Csharp/Form1.cs
+++ Emgu-Csharp/Form1.cs
@@ -16,7 +16,7 @@ using Emgu.CV.VideoSurveillance;
 using Emgu.CV.Features2D;
 using Emgu.CV.XFeatures2D;
 using Emgu.CV.Util;
-
+using Emgu.CV.Stitching;
 namespace Emgu_Csharp
 {
     public partial class Form1 : Form
@@ -27,6 +27,9 @@ namespace Emgu_Csharp
         }
         public static Image<Bgr, Byte> img1;
         public static Image<Bgr, Byte> img2;
+
+        ImageSpecifications imgD1;
+        ImageSpecifications imgD2;
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -67,12 +70,12 @@ namespace Emgu_Csharp
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ExtractFeature(img1);
-            ExtractFeature(img2);
+            ExtractFeature(img1, imgD1);
+            ExtractFeature(img2, imgD2);
 
         }
 
-        private void ExtractFeature(Image<Bgr, byte> img)
+        private void ExtractFeature(Image<Bgr, byte> img , ImageSpecifications imgD)
         {
             using (Image<Gray, Byte> model = new Image<Gray, byte>(img.Bitmap))
             using (var modelMat = model.Mat.ToUMat(AccessType.Read))
@@ -81,7 +84,40 @@ namespace Emgu_Csharp
                 var descriptor = new Mat();
                 var keyPoints = new VectorOfKeyPoint();
                 detector.DetectAndCompute(modelMat, null, keyPoints, descriptor, false);
+                imgD.Height = img.Height;
+                imgD.Width = img.Width;
+                imgD.KeyPoints = keyPoints;
+                imgD.Descriptors = descriptor;
             }
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static void Stitch2Images(Image<Bgr, byte> img1 , Image<Bgr, byte> img2)
+        {
+
+            List<Image<Bgr, byte>> imageArray = new List<Image<Bgr, byte>>();
+            imageArray.Add(img1);
+            imageArray.Add(img2);
+
+                using (var stitcher = new Stitcher(false))
+                {
+                    using (var vm = new VectorOfMat())
+                    {
+                        var result = new Mat();
+                        vm.Push(imageArray.ToArray());
+                        stitcher.Stitch(vm, result);
+
+                    }
+                }
+                foreach (Image<Bgr, Byte> img in imageArray)
+                {
+                    img.Dispose();
+                }
+        }
+
+
     }
 }
